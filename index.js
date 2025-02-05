eb806a852e747cbbd38ac93902ec97fcd4bff08b
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-02-05 19:55:13 +0500

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("I am Ahmed Abbasi");
+// FUNCTION DEFINE
+function sum(a, b) {
+    return a + b;
+}
+// FUNCTION CALL
+//* FUNCTIONS CALL
+console.log(sum(5, 10));
+
+
---
+++ index.ts
@@ -0,0 +1,32 @@
+console.log("I am Ahmed Abbasi")
+
+// FUNCTION DEFINE
+//@ts-ignore
+function sum(a:number, b:number):number {
+  return a + b;
+}
+
+// FUNCTION CALL
+
+//* FUNCTIONS CALL
+console.log(sum(5,10));
+
+let message:string = "Hello TypeScript!";
+
+// CONCATINATION
+
+let firstName:string = "Ahmed";
+let lastName:string = "Abbasi";
+
+console.log(firstName + " " + lastName);
+
+// CHECK EVEN NUMBER
+
+const  checkEvenNum=(num:number):boolean=>{
+  return num%2===0;
+}
+
+checkEvenNum(4);
+
+
+
