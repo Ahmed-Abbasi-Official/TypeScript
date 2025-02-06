400e7c2999e212bf24152339f09ad58013265d70
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-02-06 13:38:08 +0500

    array done

---
+++ array.ts
@@ -0,0 +1,29 @@
+// a)  MAKING ARRAY
+
+const number:number[]=[1,2,3,4,5];
+
+// b) USING ARRAY CONSTRUCTOR
+
+const number1:number[]=new Array(1,2,3,4,5)
+
+// c) ARRAY OF METHOD
+
+// const names:string[] = Array.of("Ahmed","Abbasi")
+
+// OPERATION : ACCESSING ELEMENTS USING INDEX
+
+console.log(number[2]);
+
+// ! MAP METHOD
+
+// ? 1: DOUNBLING EACH NUMBER  ;
+
+const doubleNumber:number[] = number.map((curVal:number)=>curVal*2);
+console.log(doubleNumber)
+
+// ? 2: NUMBER TO STRING    ;
+
+const numberToString:string[] = number.map((curVal:number)=>curVal?.toString());
+console.log(numberToString);
+
+
